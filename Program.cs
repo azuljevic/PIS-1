eab8830b884491bdd08e32febd3341708f44ff55
student <Student@B420-01.fesb.hr>
2021-03-23 18:27:04 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pis1ana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
